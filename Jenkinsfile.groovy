6162edfd73caa1de46637e4551c011aebc249744
segevb <73795011+segevb@users.noreply.github.com>
2020-12-07 22:12:23 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,13 +11,13 @@ pipeline {
                 }
             }
         }
-#        stage('Test Docker image') {
-#            steps {
-#                script {
-#                    sh "sh ./basic.test.sh"
-#                }
-#            }
-#        }
+###        stage('Test Docker image') {
+###            steps {
+###                script {
+###                    sh "sh ./basic.test.sh"
+###                }
+###            }
+###        }
         stage('Upload image to repository') {
             steps {
                 println("Input was " + userInput)
