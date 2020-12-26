8abd5c4a534b60e9131873b90ba4afcd74706b0e
Segev <segev91.b@gmail.com>
2020-12-26 17:10:12 +0200

    update test file and Build Docker image script

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,13 +24,13 @@ pipeline {
         }
         stage('Test Docker image') {
             steps {
-                    script{
-                        try {
-                            sh "./basic.test.sh segevb/weather_app:${latestVersion}-${lastCommit}"
-                        } catch (err) {
-                            println("Error thrown on test file execution")
-//                            currentBuild.result = 'ABORTED'
-//                            error('Error thrown on test file execution')
+                script{
+                    try {
+                        sh "./basic.test.sh segevb/weather_app:${latestVersion}-${lastCommit}"
+                    } catch (err) {
+                        println("Error thrown on test file execution")
+//                      currentBuild.result = 'ABORTED'
+//                      error('Error thrown on test file execution')
                         }
                     }
                 }
