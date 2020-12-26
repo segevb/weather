006cdc2fc67b1a46dffc5710ffa81d57a3e019a2
segevb <73795011+segevb@users.noreply.github.com>
2020-12-26 12:44:41 +0200

    Update send.py

--- send.py
+++ send.py
@@ -5,18 +5,17 @@ def send(key, host, city):
     logger.info("Requested city temperature for " + city)
     url = "https://community-open-weather-map.p.rapidapi.com/weather"  # open weather url endpoint
 
-    while city != "q":
-        querystring = {"q": city, "units": "metric"}  # my query
-        headers = {
-            'x-rapidapi-host': key,
-            'x-rapidapi-key': host
-        }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        if response.status_code != 200:
-            print("city not found")
-            exit()
+    #    while city != "q":
+    querystring = {"q": city, "units": "metric"}  # my query
+    headers = {
+        'x-rapidapi-host': key,
+        'x-rapidapi-key': host
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code != 200:
+        print("city not found")
+        exit()
 
-        response = response.json()
-
-        print(response["main"]["temp"])
-        city = input("Enter city: (Press q to quit) ")
+    response = response.json()
+    print("The temperature in", city, "is:", response["main"]["temp"], "°C")
+#    city = input("Enter city: (Press q to quit) ")
