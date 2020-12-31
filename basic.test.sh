9f7676ca3d97fce58c77329897c2934c78a413c2
Segev <segev91.b@gmail.com>
2020-12-31 18:16:18 +0200

    adding permmision to test file

--- basic.test.sh
+++ basic.test.sh
@@ -19,11 +19,4 @@ for city in eilat london; do
 done
 
 
-# Test if site is UP/DOWN
-if curl -s --head  --request GET https://community-open-weather-map.p.rapidapi.com/?rapidapi-key=fc03dcea31msh89ee8e25d5442c6p1f4633jsn5934736d9bb5 | grep "200 OK" > /dev/null; then
-   echo "Server is UP"
-else
-   echo "Server is down"
-fi
-
 exit 0
